6fc997091e3bd15353c26974cb5800b9463cb4b5
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-05-28 17:47:10 -0300

    [Component] Update Repository List and Item

--- src/components/RepositoryList/RepositoryItem/index.jsx
+++ src/components/RepositoryList/RepositoryItem/index.jsx
@@ -1,13 +1,17 @@
+import './style.scss';
+
 export function RepositoryItem(props) {
   return (
     <li>
       <strong>{props.repository.name ?? 'Name'}</strong>
-      <p>
-        {props.repository.description ?? 'Description'}
-      </p>
-      <a href={props.repository?.link} target="_blank">
-        Access Repository
-      </a>
+      <article>
+        <p>
+          {props.repository.description ?? 'Description'}
+        </p>
+        <a href={props.repository?.link} target="_blank">
+          Access Repository
+        </a>
+      </article>
     </li>
   );
 }
--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,6 +1,10 @@
+import './style.scss';
+
 import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
+
 import { RepositoryItem } from './RepositoryItem';
+
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([
@@ -36,19 +40,23 @@ export function RepositoryList() {
   }
   
   return (
-    <section className="repository-list">
-      <h1>Repositories</h1>
-      <ul>
-        {repositories.map(repository =>
-          <RepositoryItem
-            key={repository.id}
-            repository={repository}
-          />
-        )}
-      </ul>
-      <button type="button" onClick={AddNewProject}>
-        Add New Project
-      </button>
-    </section>
+    <aside className="repository-list">
+      <div className="repository-list-wrapper">
+        <h2>Repositories</h2>
+        <ul>
+          {repositories.map(repository =>
+            <RepositoryItem
+              key={repository.id}
+              repository={repository}
+            />
+          )}
+        </ul>
+        <div className="button-area">
+          <button type="button" onClick={AddNewProject}>
+            Add New Project
+          </button>
+        </div>
+      </div>
+    </aside>
   );
 }
