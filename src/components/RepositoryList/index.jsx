2af83b0e605e53e29e8222d2455abfdb0d06b91e
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-06-01 21:32:33 -0300

    [Component] Update RepositoryList

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -3,16 +3,17 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 
 export function RepositoryList(props) {  
+  const [user, setUser] = useState('pattonhoffiman');
   const [repositories, setRepositories] = useState([]);
 
-  useEffect(() => {
-    fetch('https://api.github.com/users/pattonhoffiman/repos')
+  useEffect(() => {    
+    fetch(`https://api.github.com/users/${user}/repos`)
     .then(res => res.json())
     .then(data => setRepositories(data));
   }, []);
 
   function getRepositoryData(repository) {
-    fetch(`https://api.github.com/repos/pattonhoffiman/${repository.name}`)
+    fetch(`https://api.github.com/repos/${user}/${repository.name}`)
     .then(res => res.json())
     .then(data => props.getData(data));
   }
@@ -21,15 +22,17 @@ export function RepositoryList(props) {
     <aside className="repository-list">
       <div className="repository-list-wrapper">
         <h2>Repositories</h2>
-        <ul>
-          {repositories.map(repository =>
-            <RepositoryItem
-              key={repository.id}
-              repository={repository}
-              buttonAction={getRepositoryData}
-            />
-          )}
-        </ul>
+          {repositories && 
+            <ul>
+              {repositories.map(repository =>
+                <RepositoryItem
+                  key={repository.id}
+                  repository={repository}
+                  buttonAction={getRepositoryData}
+                />
+              )}
+            </ul>
+          }
       </div>
     </aside>
   );
