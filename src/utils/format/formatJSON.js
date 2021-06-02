d2795bc494022a699edad0789086be8af1418e40
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-06-01 21:33:37 -0300

    [Utils] Create JSON Stringfy Formatter

---
+++ src/utils/format/formatJSON.js
@@ -0,0 +1,19 @@
+export default function formatJSON(string) {
+  if(string) {
+    return splitByComma(
+      removeDoubleComma(
+        removeDoubleDots(
+          removeNumbers(
+            removeKeys(string)
+          )
+        )
+      )
+    );
+  }
+}
+
+const splitByComma = (string) => string.split(",");
+var removeDoubleDots = (string) => string.replaceAll(":", "");
+const removeNumbers = (string) => string.replaceAll(/[0-9]/g, "");
+const removeDoubleComma = (string) => string.replaceAll(/"/g, "");
+const removeKeys = (string) => string.replace("{", "").replace("}", "");
