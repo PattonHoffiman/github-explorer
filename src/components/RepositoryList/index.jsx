f89e33a0b328c7cc575681d70881221e8d26db05
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-06-02 23:41:51 -0300

    [Component] Update RepositoryList

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,39 +1,39 @@
 import './style.scss';
 import { useState, useEffect } from 'react';
-import { RepositoryItem } from './RepositoryItem';
+import RepositoryItem from './RepositoryItem';
 
-export function RepositoryList(props) {  
-  const [user, setUser] = useState('pattonhoffiman');
+export default function RepositoryList(props) {  
   const [repositories, setRepositories] = useState([]);
 
-  useEffect(() => {    
-    fetch(`https://api.github.com/users/${user}/repos`)
+  useEffect(() => {
+    fetch(`https://api.github.com/users/pattonhoffiman/repos`)
     .then(res => res.json())
-    .then(data => setRepositories(data));
+    .then(data => {
+      setRepositories(data);
+      console.log(data);
+    });
   }, []);
 
   function getRepositoryData(repository) {
-    fetch(`https://api.github.com/repos/${user}/${repository.name}`)
-    .then(res => res.json())
-    .then(data => props.getData(data));
+    props.getRepositoryName(repository);
   }
   
   return (
     <aside className="repository-list">
-      <div className="repository-list-wrapper">
-        <h2>Repositories</h2>
-          {repositories && 
-            <ul>
-              {repositories.map(repository =>
-                <RepositoryItem
-                  key={repository.id}
-                  repository={repository}
-                  buttonAction={getRepositoryData}
-                />
-              )}
-            </ul>
-          }
-      </div>
+      {repositories[0] &&
+        <div className="repository-list-wrapper">
+          <h2>Repositories</h2>
+          <ul>
+            {repositories.map(repository =>
+              <RepositoryItem
+                key={repository.id}
+                repository={repository.name}
+                buttonAction={getRepositoryData}
+              />
+            )}
+          </ul>
+        </div>
+      }
     </aside>
   );
 }
